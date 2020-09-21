8c14d0ed95673b84c873b28920a32b6007a7708e
Jari Poutanen <jj.poutanen@gmail.com>
2020-09-21 15:13:20 +0300

    Es lint tarkastettu versio

--- models/contact.js
+++ models/contact.js
@@ -1,37 +1,36 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+
 mongoose.set('useCreateIndex', true);
+const connString = process.env.MONGO_URL;
 
-const connString = process.env.MONGO_URL 
-
-console.log(connString)
-mongoose.connect(connString,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => console.log("Connected to Puhelinluettelo database"))
-.catch((error) => console.log(`Error connecting to Puhelinluettelo database:`,error.message))
+console.log(connString);
+mongoose.connect(connString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to Puhelinluettelo database'))
+  .catch((error) => console.log('Error connecting to Puhelinluettelo database:', error.message));
 
 const contactSchema = new mongoose.Schema({
-    name: {
+  name: {
     type: String,
     minlength: 3,
     required: true,
-    unique: true
-    },
-    number: {
-      type: String,
-      minlength: 8,
-      required: true
-    }
-  })
+    unique: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
+});
 
-
-  contactSchema.set('toJSON', {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 contactSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model("Contact",contactSchema)
+module.exports = mongoose.model('Contact', contactSchema);
